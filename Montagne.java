c22ad210e042c6ff14f41dbf5f359e3516fc0029
jlai083 <jlai083@live.fr>
2017-03-17 20:50:15 +0100

    Ajout de Montagne.java

---
+++ Montagne.java
@@ -0,0 +1,22 @@
+class Montagne
+{
+	private String mont;
+	private int altitude;
+
+	Montagne(nom, prenom, age)
+	{
+		this.mont=mont;
+		this.altitude=altitude;
+	}
+
+	public String getMont()
+	{
+		return this.mont;
+	}
+
+	public String getAltit()
+	{
+		return this.altit;
+	}
+
+}
